049f136e8cd558036789951cdae8fc1e89be2bb7
Michael Neely <michael@michaelneely.net>
2023-12-18 09:47:14 -0500

    Got ready for IndexedDB lecture coming up next

---
+++ app.js
@@ -0,0 +1,8 @@
+const storeBtn = document.getElementById('store-btn');
+const retrBtn = document.getElementById('retrieve-btn');
+storeBtn.addEventListener('click', () => {
+
+});
+retrBtn.addEventListener('click', () => {
+
+});
